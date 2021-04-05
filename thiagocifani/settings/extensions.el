e96ad1843bea4d314eb6278aa2bcf886648133cb
thiagocifani <cifani.thiago@gmail.com>
2021-04-05 12:46:41 -0300

    Add extensions

---
+++ thiagocifani/settings/extensions.el
@@ -0,0 +1,26 @@
+;; Ruby
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+;; YAML
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; Markdown
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+;; HTML
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+
+;; CSS, SCSS
+(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.scss.erb$" . scss-mode))
+
+;; Scala
+(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.sc$" . scala-mode))
+
+;; C/C++
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
