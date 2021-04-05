c1f71527b36793cb32a9b6b9103cfd6e6489caa6
thiagocifani <cifani.thiago@gmail.com>
2021-04-05 13:00:35 -0300

    Add custom js config

---
+++ thiagocifani/custom/js-config.el
@@ -0,0 +1,46 @@
+(use-package js2-mode
+  :commands js2-mode
+  :mode
+  ("\\.js$" . js2-mode)
+  ("\\.jsx$" . js2-mode)
+  ("\\.es6$" . js2-mode)
+  :config
+  (use-package js2-refactor
+    :commands (js2r-add-keybindings-with-prefix)
+    :config
+    (add-hook 'js2-mode-hook #'js2-refactor-mode)
+    (add-hook 'js2-mode-hook #'thiagocifani-tide-setup)
+    (js2r-add-keybindings-with-prefix "C-c t"))
+
+  (use-package add-node-modules-path
+    :commands add-node-modules-path
+    :config
+    (add-hook 'js2-mode-hook #'add-node-modules-path))
+
+  (setq js2-basic-offset 2)
+  (setq js2-global-externs '("module" "require" "console" "JSON" "$" "_")))
+
+(use-package jest
+  :bind
+  ("C-c , v" . jest-file)
+  ("C-c , s" . jest-function)
+  ("C-c , t" . thiagocifani/jest-jump-between-test-and-impl))
+
+(use-package tern
+  :commands tern-mode
+  :config
+  (use-package company-tern
+    :config
+    (use-package tern-auto-complete
+      :config
+      (with-eval-after-load 'company
+        (require 'company-tern)
+        (require 'tern-auto-complete)
+        (add-to-list 'company-backends 'company-tern)))))
+
+(use-package prettier-js
+  :commands prettier-js-mode)
+
+(use-package rjsx-mode)
+
+(provide 'js-config)
