d2fd03731bd3ba74f7656a27b159dd36dc4ef860
thiagocifani <cifani.thiago@gmail.com>
2021-04-05 12:46:00 -0300

    Add packages config, install use-package by default

---
+++ thiagocifani/init/packages.el
@@ -0,0 +1,17 @@
+(require 'cl)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;;Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
